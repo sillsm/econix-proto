7692cbeea7373080d776927640bd52e6c30bf730
Max Sills <sillsm@gmail.com>
2015-07-25 19:47:57 +0000

    Start to track agents in dev server.

--- devserver/server.go
+++ devserver/server.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
 
+type agent struct {
+	name  string
+	value int
+}
+
+var agents []agent
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	for _, a := range agents {
+		io.WriteString(w,
+			fmt.Sprintf("Agent: %v, Val:%v\n", a.name, a.value))
+	}
 }
 
 func main() {
+	agents = []agent{
+		{"A", 4},
+		{"B", 0},
+		{"Bank", 50},
+	}
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8080", nil)
 }
