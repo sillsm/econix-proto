01b0b1419d38cdc1386087749dc0035bda68a324
Max Sills <sillsm@gmail.com>
2015-08-30 00:29:54 +0000

    bring back the proto

--- get.sh
+++ get.sh
@@ -1,2 +1,4 @@
 # Copies entire working repo into proper $GOPATH.
+mkdir $GOPATH/src/github.com/sillsm
+mkdir $GOPATH/src/github.com/sillsm/econix-proto/
 cp -R * $GOPATH/src/github.com/sillsm/econix-proto/
